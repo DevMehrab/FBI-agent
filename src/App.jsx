f5f3d8af9fe91ec3a5dff85b367a99b9b7c259cb
Mehrab Hossain <dev.mehrabhossain@gmail.com>
2025-04-11 20:32:14 +0600

    changed html title

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { PasswordProvider } from "./components/PasswordProvider";
 function App() {
   return (
     <PasswordProvider>
-      <div className="h-screen flex justify-center items-center bg-neutral-900 p-3 flex-col">
+      <div className="h-screen flex justify-center items-center bg-neutral-900 p-4 flex-col">
         <h1 className="absolute top-0 p-4 text-xl uppercase text-neutral-800 font-bold">Password Generator</h1>
         <div className="flex justify-center items-start flex-col w-screen max-w-96">
           <Output />
